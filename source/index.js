7fa6ccd4b4b28f07ae2c2785926451888041b174
Rita Akhmadulina <rita.akhmadulina@gmail.com>
2019-05-17 20:24:07 +0300

    finish

--- source/index.js
+++ source/index.js
@@ -2,6 +2,8 @@
 import './rateit';
 import 'social-likes';
 import 'social-likes/dist/social-likes_birman.css';
+import '@claviska/jquery-minicolors/jquery.minicolors.css';
+import '@claviska/jquery-minicolors';
 
 import 'ress';
 import './styles/_index.sass';
@@ -10,7 +12,9 @@ $(document).ready(function() {
   const $languageEl = $('.language__el'),
         $language = $('.language'),
         $filter = $('.filter__btn'),
-        $uploadFile = $('#input-file');
+        $uploadFile = $('#input-file'),
+        $uploadFont = $('#input-font'),
+        $uploadIllustration = $('#input-illustration');
 
   //Language
   $language.hover(function(){
@@ -29,32 +33,43 @@ $(document).ready(function() {
   });
 
   //input file
-  $uploadFile.change(function(){
-    const label = $(this).parent().find('span');
-    if(typeof(this.files) !='undefined'){ // fucking IE
-      if(this.files.length == 0){
-        label.removeClass('withFile').text(label.data('default'));
+  $uploadFile.change(changeInputFile);
+  $uploadFont.change(changeInputFile);
+  $uploadIllustration.change(changeInputFile);
+
+
+  // color picker
+  $('INPUT.minicolors').minicolors({
+    defaultValue: '#000000'
+  });
+
+
+  // $('#colorpicker').on('change', function() {
+  //   $('#hexcolor').val(this.value);
+  // });
+  // $('#hexcolor').on('change', function() {
+  //   $('#colorpicker').val(this.value);
+  // });
+
+  function changeInputFile(){
+    const $label = $(this).parent().find('span');
+    const $title = $(this).parent().parent().find('.request__file-selected');
+
+    if (typeof(this.files) !== 'undefined'){ // fucking IE
+      if (this.files.length === 0){
+        $label.removeClass('withFile').text($label.data('default'));
+      } else {
+        const fileName = this.files[0].name;
+
+        if (fileName) {
+          $($title).text(fileName);
+        }
       }
-      else{
-        const file = this.files[0];
-        const name = file.name;
-        label.addClass('withFile').text(name);
-        $('.request__file-selected').addClass('is-hidden')
-      }
-    }
-    else{
+    } else {
       const name = this.value.split("\\");
-      label.addClass('withFile').text(name[name.length-1]);
+      // $label.addClass('withFile').text(name[name.length-1]);
     }
+
     return false;
-  });
-
-  $('#colorpicker').on('change', function() {
-    $('#hexcolor').val(this.value);
-  });
-  $('#hexcolor').on('change', function() {
-    $('#colorpicker').val(this.value);
-  });
-
-
+  }
 });
